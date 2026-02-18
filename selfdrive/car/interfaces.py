91a56d9bd13d13452467dd1964c5709e289cc17d
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-02-19 08:39:25 +0900

    Comment out event checks in create_common_events method
    Comment out event checks for door open, seatbelt unlatched, and wrong gear.

--- selfdrive/car/interfaces.py
+++ selfdrive/car/interfaces.py
@@ -134,13 +134,13 @@ class CarInterfaceBase(ABC):
   def create_common_events(self, cs_out, extra_gears=None, pcm_enable=True):
     events = Events()
 
-    if cs_out.doorOpen:
-      events.add(EventName.doorOpen)
-    if cs_out.seatbeltUnlatched:
-      events.add(EventName.seatbeltNotLatched)
-    if cs_out.gearShifter != GearShifter.drive and (extra_gears is None or
-       cs_out.gearShifter not in extra_gears):
-      events.add(EventName.wrongGear)
+    #if cs_out.doorOpen:
+    #  events.add(EventName.doorOpen)
+    #if cs_out.seatbeltUnlatched:
+    #  events.add(EventName.seatbeltNotLatched)
+    #if cs_out.gearShifter != GearShifter.drive and (extra_gears is None or
+    #   cs_out.gearShifter not in extra_gears):
+    #  events.add(EventName.wrongGear)
     if cs_out.gearShifter == GearShifter.reverse:
       events.add(EventName.reverseGear)
     if not cs_out.cruiseState.available:
