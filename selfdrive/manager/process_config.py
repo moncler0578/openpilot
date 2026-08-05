9c58dc8dd7fd279fb61a0bed30e8ccf8cdd9e040
moncler0578 <93327539+moncler0578@users.noreply.github.com>
2026-08-06 07:25:39 +0900

    start USB GPS daemon on EON

--- selfdrive/manager/process_config.py
+++ selfdrive/manager/process_config.py
@@ -40,6 +40,7 @@ procs = [
   #PythonProcess("statsd", "selfdrive.statsd", persistent=True),
 
   # EON only
+  PythonProcess("usb_gpsd", "selfdrive.locationd.usb_gpsd", enabled=EON, persistent=True),
   PythonProcess("rtshield", "selfdrive.rtshield", enabled=EON),
   PythonProcess("shutdownd", "selfdrive.hardware.eon.shutdownd", enabled=EON),
   PythonProcess("androidd", "selfdrive.hardware.eon.androidd", enabled=EON, persistent=True),
